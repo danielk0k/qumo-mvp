8ae4e2b2dd5bd52188044a4ad121dd63b36bc22e
danielk0k <70556073+danielk0k@users.noreply.github.com>
2024-02-23 18:01:39 +0800

    First commit

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,19 @@
+import { StreamingTextResponse } from "ai";
+import toReadableStream from "to-readable-stream";
+
+const questions = [
+  "What do you enjoy doing in your free time?",
+  "Have you traveled anywhere recently, and if so, what was your favorite part of the trip?",
+  "What's your go-to book, movie, or TV show recommendation?",
+  "If you could have dinner with any historical figure, living or dead, who would it be and why?",
+  "Do you have any upcoming exciting plans or events that you're looking forward to?"
+];
+
+export async function POST(req: Request) {
+  const { messages } = await req.json();
+  const i = (messages.length / 2) - 1
+  if (i > questions.length) {
+    return new StreamingTextResponse(toReadableStream("Thank you for your participation. You may close this window."));
+  }
+  return new StreamingTextResponse(toReadableStream(questions[i]));
+}
---
+++ src/app/chat/[code]/page.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { useChat } from "ai/react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { useEffect, useRef } from "react";
+
+const welcome_msg =
+  "Hey, nice meeting you today! To get started, please type 'ready' and I will begin asking you some questions for this research.";
+
+export default function Chat({ params }: { params: { code: string } }) {
+  const contentRef = useRef<HTMLDivElement>(null);
+  const { messages, input, handleInputChange, handleSubmit } = useChat({
+    initialMessages: [{ role: "assistant", content: welcome_msg, id: "" }],
+  });
+
+  const handleScroll = () => {
+    if (contentRef.current) {
+      contentRef.current.scrollTop = contentRef.current.scrollHeight;
+    }
+  };
+
+  useEffect(handleScroll, [messages]);
+
+  return (
+    <main className="flex flex-col mx-auto max-w-2xl justify-center p-16">
+      <Card>
+        <CardHeader>
+          <CardTitle>Test Research</CardTitle>
+          <CardDescription>5 questions to get to know you</CardDescription>
+        </CardHeader>
+        <CardContent className="max-h-96 overflow-y-auto mb-8" ref={contentRef}>
+          {messages.map((m) => (
+            <div key={m.id} className="whitespace-pre-wrap my-2">
+              <p className="font-bold">{m.role === "user" ? "User" : "Qumo"}</p>
+              <p className="mt-1">{m.content}</p>
+            </div>
+          ))}
+        </CardContent>
+        <form onSubmit={handleSubmit}>
+          <CardFooter>
+            <Input
+              value={input}
+              placeholder="Enter your response here"
+              onChange={handleInputChange}
+            />
+          </CardFooter>
+        </form>
+      </Card>
+    </main>
+  );
+}
